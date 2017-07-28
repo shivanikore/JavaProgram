b78e92eaf8a6fdae4338e4c4714ae25423d4841e
shivanikore <shivanikore>
2017-07-28 19:16:54 +0530

    My second commit

---
+++ src/sample.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Trupti on 7/27/2017.
+ */
+public class sample {
+    public static void main(String args[]) {
+        int a,b,c;
+        a=2;
+        b=3;
+        c=a+b;
+
+        System.out.println("addition of two nos"+c);
+    }
+}
